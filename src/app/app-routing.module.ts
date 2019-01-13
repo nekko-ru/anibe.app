585ddbce3c7583e8e2ae8641462862a9280482b9
deissh <kudryavtsev@nppsatek.ru>
2019-01-13 21:40:10 +0300

    добавил guards для авторизации + исправил шаблон

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { IntroGuard } from './guards/intro.guard';
+import { LoginGuard } from './guards/login.guard';
 
 const routes: Routes = [
   { path: '', loadChildren: './tabs/tabs.module#TabsPageModule', canActivate: [IntroGuard] },
   { path: 'intro', loadChildren: './intro/intro.module#IntroPageModule' },
   { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
-  { path: 'profile', loadChildren: './profile/profile.module#ProfilePageModule' },
+  { path: 'profile', loadChildren: './profile/profile.module#ProfilePageModule', canActivate: [LoginGuard] },
   { path: 'viewer', loadChildren: './viewer/viewer.module#ViewerPageModule' },
   { path: 'search-results', loadChildren: './search-results/search-results.module#SearchResultsPageModule' },
-  { path: 'search-params', loadChildren: './search-params/search-params.module#SearchParamsPageModule' },
   { path: 'info/:id', loadChildren: './info/info.module#InfoPageModule' },
   { path: 'reader/:id', loadChildren: './reader/reader.module#ReaderPageModule' },
-  { path: 'select-chapter', loadChildren: './select-chapter/select-chapter.module#SelectChapterPageModule' }
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
