5f02c2ebf1cca376f41ec67f64beb2ddf6389fdd
deissh <kudryavtsev@nppsatek.ru>
2019-01-15 20:35:02 +0300

    добавил тостер при авторизации

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Storage } from '@ionic/storage';
 import { UserService } from 'src/app/providers/user.service';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -15,15 +16,27 @@ export class LoginPage implements OnInit {
   constructor(
     private storage: Storage,
     private router: Router,
-    private user: UserService
+    private user: UserService,
+    private toastController: ToastController
   ) { }
 
   ngOnInit() {
   }
 
   async login() {
-    this.user.setAuth(this.username, this.password);
-    const data = await this.user.auth(this.username, this.password);
+    const toast = await this.toastController.create({
+      message: `Неверный адрес электронной почты или пароль`,
+      duration: 5000
+    });
+
+    let data = { token: null };
+    try {
+      this.user.setAuth(this.username, this.password);
+      data = await this.user.auth(this.username, this.password);
+    } catch (e) {
+      console.log(e);
+      await toast.present();
+    }
 
     await this.storage.set('token', data.token);
     this.router.navigateByUrl('/');
