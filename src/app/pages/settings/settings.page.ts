11d0cbe48c8e9bdcca341b34d3b08b6b4d5aaca7
deissh <kudryavtsev@nppsatek.ru>
2019-01-31 15:34:24 +0300

    добавил переключатели + правки

--- src/app/pages/settings/settings.page.ts
+++ src/app/pages/settings/settings.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/providers/user.service';
+import { Router } from '@angular/router';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'app-settings',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./settings.page.scss'],
 })
 export class SettingsPage implements OnInit {
+  public info: any;
+  public showProfile: boolean;
 
-  constructor() { }
+  constructor(
+    private user: UserService,
+    private storage: Storage,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.load();
+  }
+
+  private async load() {
+    this.info = await this.storage.get('user_local');
+    this.showProfile = this.info ? true : false;
   }
 
 }
