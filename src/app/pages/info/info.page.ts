a2c4018d54a600a3ab1331bb2d90eeba2545b6fe
deissh <kudryavtsev@nppsatek.ru>
2019-02-15 22:24:10 +0300

    добавил кеширование страницы с информацией манги

--- src/app/pages/info/info.page.ts
+++ src/app/pages/info/info.page.ts
@@ -4,6 +4,7 @@ import { LoadingController, ModalController, PopoverController, ActionSheetContr
 import { IPostFull } from 'src/app/providers/interfaces';
 import { PostService } from 'src/app/providers/post.service';
 import { Firebase } from '@ionic-native/firebase/ngx';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'app-info',
@@ -23,6 +24,7 @@ export class InfoPage implements OnInit {
     private router: Router,
     private loadingController: LoadingController,
     private post: PostService,
+    private storage: Storage,
     private asc: ActionSheetController,
     private firebase: Firebase
   ) {}
@@ -40,8 +42,15 @@ export class InfoPage implements OnInit {
     await this.firebase.logEvent('select_content', { item_id: this.id, content_type: 'manga' });
   }
 
-  private async load() {
-    this.info = await this.post.get(this.id);
+  private async load(full: boolean = false) {
+    const temp = await this.storage.get(`info_${this.id}`);
+    if (temp && !full) {
+      this.info = temp;
+    } else {
+      Object.assign(this.info, await this.post.get(this.id));
+      await this.storage.set(`info_${this.id}`, this.info);
+    }
+
     await this.spiner.dismiss();
   }
 
